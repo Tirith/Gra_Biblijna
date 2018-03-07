f5aa3fb7fdc6ae0ecaf862b026486c01a28b5bec
Tirith <boructomek@gmail.com>
2018-03-07 19:10:21 +0100

    0.0.2
    - uruchomienie autoryzacji
    - rejestracja tylko przez admina
    - zrobienie migracji nowej tabeli z pytaniami

---
+++ database/migrations/2018_03_07_180246_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('pytanie');
+            $table->string('odp1');
+            $table->string('odp2');
+            $table->string('odp3');
+            $table->string('odp4');
+            $table->integer('poprawna');
+            $table->integer('trudnosc');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,14 +72,14 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   TU BEDZIE GRA
                 </div>
 
                 <div class="links">
