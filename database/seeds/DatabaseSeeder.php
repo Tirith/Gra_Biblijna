e1020f502168d3da058e3405414a721228679ec6
Tirith <boructomek@gmail.com>
2018-03-07 21:31:23 +0100

    0.0.3a
    -Dodanie seeda pytań

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,17 @@ class DatabaseSeeder extends Seeder
 								'name' => 'Tomasz Boruc',								
 								'email' => 'boructomek@gmail.com',							
 								'password' => bcrypt('pass'),	]);
+
+        DB::table('questions')->insert([
+                                'pytanie' => 'Kto zbudował arkę?',                               
+                                'odp1' => 'Mojżesz',                          
+                                'odp2' => 'Noe',                          
+                                'odp3' => 'Jezus',                          
+                                'odp4' => 'Judasz',                          
+                                'poprawna' => 2,                          
+                                'trudnosc' => 1,                          
+                                   ]);
     }
+
+    
 }
